ec61d7fd2390eaa44d7e52991702041c371184ac
FelipeCarrascoLopez <felipecarrascolopez@gmail.com>
2024-03-21 11:22:25 -0300

    correccion status home y show

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,6 +2,6 @@ class Device < ApplicationRecord
   belongs_to :store
 
   enum device_type: { printer: 0, web_server: 1, database_server: 2 }
-  enum status: [:online, :offline, :maintenance]
+  enum status: [:ok, :warning, :error]
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@ store1 = Store.create(name: "Restaurant 1")
 store2 = Store.create(name: "Restaurant 2")
 
 # Create devices for Store 1
-store1.devices.create(name: "Printer 1 - #{store1.name}", device_type: :printer, status: :online, last_status_update: Time.now)
-store1.devices.create(name: "Web Server 1 - #{store1.name}", device_type: :web_server, status: :online, last_status_update: Time.now)
-store1.devices.create(name: "Database Server 1 - #{store1.name}", device_type: :database_server, status: :online, last_status_update: Time.now)
+store1.devices.create(name: "Printer 1 - #{store1.name}", device_type: :printer, status: :ok, last_status_update: Time.now)
+store1.devices.create(name: "Web Server 1 - #{store1.name}", device_type: :web_server, status: :ok, last_status_update: Time.now)
+store1.devices.create(name: "Database Server 1 - #{store1.name}", device_type: :database_server, status: :ok, last_status_update: Time.now)
 
 # Create devices for Store 2
-store2.devices.create(name: "Printer 1 - #{store2.name}", device_type: :printer, status: :online, last_status_update: Time.now)
-store2.devices.create(name: "Web Server 1 - #{store2.name}", device_type: :web_server, status: :online, last_status_update: Time.now)
-store2.devices.create(name: "Database Server 1 - #{store2.name}", device_type: :database_server, status: :online, last_status_update: Time.now)
+store2.devices.create(name: "Printer 1 - #{store2.name}", device_type: :printer, status: :error, last_status_update: Time.now)
+store2.devices.create(name: "Web Server 1 - #{store2.name}", device_type: :web_server, status: :error, last_status_update: Time.now)
+store2.devices.create(name: "Database Server 1 - #{store2.name}", device_type: :database_server, status: :error, last_status_update: Time.now)
